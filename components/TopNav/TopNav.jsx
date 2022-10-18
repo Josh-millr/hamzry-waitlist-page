37a5d26d162750f6ebb818f3b16f77ae81a09486
josh-miller <joshuae.miller100@gmail.com>
2022-10-18 12:38:44 +0100

    Created the top Navigation

--- components/TopNav/TopNav.jsx
+++ components/TopNav/TopNav.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export const TopNav = () => {
+  let [currPath, setCurrPath] = useState("/");
+
+  useEffect(() => setCurrPath(window.location.pathname), [setCurrPath]);
   return (
     <nav>
       <div className="flex w-full items-center justify-between py-[24px] px-[24px] sm:py-[40px] sm:px-[72px]">
         {/* Column-1 */}
-        <Image
-          src="/hamzry-logo-full.svg"
-          height={34}
-          width={134}
-          alt="Hamzry Logo"
-        />
+        <Link href="/">
+          <Image
+            src="/hamzry-logo-full.svg"
+            height={34}
+            width={134}
+            alt="Hamzry Logo"
+          />
+        </Link>
         {/* Column-2 */}
         <ul className="flex gap-x-[24px] text-[14px] font-bold">
+          {/* // TODO: Add active and inactive state style */}
           <li>
             <Link href="">Blog</Link>
           </li>
