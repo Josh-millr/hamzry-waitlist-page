2d64764890956a446fe701c15d2fd07494d9e789
josh-millr <joshuae.miller100@gmail.com>
2022-10-18 16:51:02 +0000

    Added ExtraBold font and font rule

---
+++ components/TextInput/TextInput.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const TextInput = () => {
+  return (
+    <span className="grid">
+      <span className="relative left-[20px] top-[8px] w-[fit-content] bg-[#FFFFFF] px-[8px]">
+        <label for="email" className="text-[12px] font-bold text-[#6E6E6E]">
+          Email
+        </label>
+      </span>
+      <input
+        type="text"
+        id="email"
+        className="h-[50px] w-full rounded-[8px] border-[1px] border-[#A3A3A3] px-[16px] outline-none focus:bg-[#e6fffa]"
+        placeholder="eg. joshuae.miller100@gmail.com"
+      />
+    </span>
+  );
+};
--- components/index.js
+++ components/index.js
@@ -1,2 +1,4 @@
 export { TopNav } from "./TopNav/TopNav";
 export { Hero } from "./Hero/Hero";
+
+export { TextInput } from "./TextInput/TextInput";
